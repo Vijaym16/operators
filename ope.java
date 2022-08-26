1bfc3355d527a6021ffb5d0110bac938ab1f89f4
VIJAY M <78839120+Vijaym16@users.noreply.github.com>
2022-08-26 23:06:12 +0530

    Add files via upload

---
+++ ope.java
@@ -0,0 +1,16 @@
+import java.sql.Blob;
+
+public class ope {
+    public static void main(String[] args) {
+        int a = 10; int b = 10;
+
+        int c = a+b;
+        int d = a-b;
+        int e = a*b;
+        int f = a/b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
